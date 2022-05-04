c7cc8c7e17d95ad8f370aa67278cfc7d2b54acb9
Kanat <abdsattar02@inbox.ru>
2022-05-05 02:20:35 +0600

    Edited4

--- src/main/java/kanat/gb/Cat.java
+++ src/main/java/kanat/gb/Cat.java
@@ -4,6 +4,7 @@ public class Cat {
     private String name;
     private int appetite;
     private boolean satiety;
+    private static int period = 0;
 
     public boolean isSatiety() {
         return satiety;
@@ -17,20 +18,32 @@ public class Cat {
         return appetite;
     }
 
+    public static int getPeriod() {
+        return period;
+    }
+
     public Cat(String name, int appetite) {
         this.name = name;
         this.appetite = appetite;
     }
-    public void eat(Plate plate){
-        if (plate.getFood() < appetite){
-            System.out.println("Not enough food");
-            plate.addFood(appetite);
-        }
-        if(appetite <= plate.getFood()) satiety = true;
-        else if (appetite > plate.getFood()) System.out.println("Cat " + this.name + " hungry!");
-        plate.decreaseFood(appetite);
-        System.out.println("Cat " + this.name +" full");
-        System.out.println("");
-    }
 
+    public void eat(Plate plate) {
+        period++;
+        if (period>3){
+            System.out.println("stop");
+        }else {
+            if (plate.getFood() < appetite) {
+                System.out.println("Not enough food");
+                plate.addFood(appetite);
+            }
+            if (appetite <= plate.getFood()) satiety = true;
+            else if (appetite > plate.getFood()) System.out.println("Cat " + this.name + " hungry!");
+            plate.decreaseFood(appetite);
+            System.out.println("Cat " + this.name + " full");
+            System.out.println("");
+            if (period > 3) {
+                System.out.println("stop");
+            }
+        }
+    }
 }
